505d2564b26a9fc033f7546564e25ec00300664d
GiggleFizz <wizz_fizz77@hotmail.com>
2025-06-15 10:52:31 +1000

    clock speed set to 60

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ import pygame
 from constants import *
 
 def main():
+
+
+
     pygame.init()
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    loop_start = 1
-    while loop_start != 1:
-        pygame.surface.fill("black")
-        pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    
+    loop_start = 1
+    while loop_start == 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
 
 if __name__ == "__main__":
     main()
