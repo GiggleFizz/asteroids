0754a35c72240354694eebe872d94b1e8a6eaf9d
GiggleFizz <wizz_fizz77@hotmail.com>
2025-06-15 12:02:46 +1000

    player rotation

--- main.py
+++ main.py
@@ -3,29 +3,31 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
-
-
 
     pygame.init()
     dt = 0
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     
-    loop_start = 1
-    while loop_start == 1:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black")
-        pygame.display.flip()
         dt = clock.tick(60) / 1000
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        
         
 
 
